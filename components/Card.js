ab3021cdc061249915868bd8aa91274de55351fa
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2020-10-22 19:31:54 +0300

    Create reusable Card component

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "#fff",
+    elevation: 5,
+    padding: 15,
+    borderRadius: 10,
+  },
+});
+
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-const StartGameScreen = (props) => {
+import Card from "../components/Card";
+
+const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.card}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -24,18 +26,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 15,
   },
-  inputContainer: {
+  card: {
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: "#fff",
-    elevation: 5,
-    padding: 15,
-    borderRadius: 10,
   },
   screen: {
     flex: 1,
